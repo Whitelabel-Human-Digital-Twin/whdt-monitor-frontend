af8a05d029a3869460431f1754a06bd65bf03b6b
LEONARDO MICELLI <leonardomicelli@gmail.com>
2026-03-17 11:46:58 +0100

    fix: add persistence and creation services uri rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*", // intercept /api/* calls
+        source: "/api/creation/:path*", // intercept /api/* calls
         destination: "http://localhost:8080/api/:path*", // proxy to Ktor backend
+      },
+      {
+        source: "/api/persistence/:path*", // intercept /api/* calls
+        destination: "http://localhost:8081/api/:path*", // proxy to Ktor backend
       },
     ];
   },
