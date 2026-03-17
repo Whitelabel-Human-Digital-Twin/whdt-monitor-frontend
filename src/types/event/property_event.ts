3edbf26d5ad272862f7256148256e1c9b9506f6e
LEONARDO MICELLI <leonardomicelli@gmail.com>
2026-03-17 11:38:41 +0100

    feat: add document types

---
+++ src/types/event/property_event.ts
@@ -0,0 +1,21 @@
+export interface PropertyEventMetadata {
+    hdtId: string
+    modelId: string
+    propertyName: string
+    propertyId: string
+}
+
+type PropertyValue =
+  | { type: "empty-value"; value: null }
+  | { type: "string-value"; value: string }
+  | { type: "int-value"; value: number }
+  | { type: "float-value"; value: number }
+  | { type: "boolean-value"; value: boolean }
+  | { type: "double-value"; value: number }
+  | { type: "long-value"; value: number };
+
+export interface PropertyEventDocument {
+    metaField: PropertyEventMetadata
+    timeField: string
+    value: PropertyValue
+}
